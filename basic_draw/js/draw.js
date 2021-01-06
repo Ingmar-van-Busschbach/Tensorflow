3e1ba375ee4709ba8ba2e61c6d6d2c2c5931ee82
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2021-01-06 10:43:49 +0100

    Added calculator implementation

--- basic_draw/js/draw.js
+++ basic_draw/js/draw.js
@@ -1,44 +1,27 @@
 let textHere = document.getElementById('textHere');// display data
 
-const canvas1 = document.getElementById('canvas1');
-const ctx1 = canvas1.getContext('2d');
-
-canvas1.style.zIndex = 2;
-canvas1.width = screen.width;
-canvas1.height = screen.height;
-canvas1.style.zIndex = 2;
-ctx1.translate(screen.width, 0); // flip screen horizontal
-ctx1.scale(-1, 1); // flip screen horizontal
-
-ctx1.lineJoin = 'round';
-ctx1.lineCap = 'round';
-ctx1.lineWidth = 5;
-ctx1.strokeStyle = '#ac0000';
+var elt = document.getElementById('calculator');
+var calculator = Desmos.GraphingCalculator(elt);
 
 let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-
-const clearDrawing =() => {
-  ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
-}
-
+let a = 10;
+let b = 10;
+var input = 'r=' + String(a) + '\\cos(\\theta*' + String(b) + ')*' + String(a) + '\\sin\(\\theta*' + String(b) + ')'
+calculator.setExpression({ id: 'graph1', latex: input });
 canvas2.addEventListener('mousedown', () => isDrawing = true);
 canvas2.addEventListener('mouseup', () => isDrawing = false);
 
 function drawPosenet(XY) {
   //XY = object   { x: value, y: value }
-  textHere.innerHTML= Math.floor(XY.x) + " " + Math.floor(XY.y);
-  
+  textHere.innerHTML= "Coordinates: " + Math.floor(XY.x) + ", " + Math.floor(XY.y) + ", A/B: " + a + ", " + b;
+
 
   // stop the function if not mouse down
   if(!isDrawing) return;
- 
-  //console.log(XY);
-  ctx1.beginPath();
-  ctx1.moveTo(lastX, lastY);
-  ctx1.lineTo(XY.x,XY.y);
-  ctx1.stroke();
-  [lastX, lastY] = [XY.x, XY.y];
-}
 
+  //console.log(XY);
+  a = XY.x / screen.width * 5;
+  b = XY.y / screen.height * 5;
+  input = 'r=' + String(a) + '\\cos(\\theta*' + String(b) + ')*' + String(a) + '\\sin\(\\theta*' + String(b) + ')'
+  calculator.setExpression({ id: 'graph1', latex: input });
+}
